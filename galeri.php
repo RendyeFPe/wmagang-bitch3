aa0f1bf70d485cfd7c878a38129b9f7b40654db3
RendyeFPe <putrarendy27@gmail,com>
2024-09-23 13:12:38 +0700

    update deskripsi

--- galeri.php
+++ galeri.php
@@ -3,7 +3,7 @@
 
 <head>
 
-     <title>DISPERDAGIN | Dinas Perdagangan Dan Industri</title>
+     <title>DISPERDAGIN | Dinas Perdagangan Dan Industri Rendy</title>
 
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=Edge">
